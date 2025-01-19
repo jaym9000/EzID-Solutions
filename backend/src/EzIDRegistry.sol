34f99899cb2341a517550eabe482eff0570358bc
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2025-01-19 10:13:06 -0400

    chore: forge init

---
+++ backend/src/EzIDRegistry.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import {console2} from "forge-std/console2.sol";
+
+contract EzIDRegistry  {
+    // constructor() ERC721("EzIDRegistry", "EZID") {}
+}
